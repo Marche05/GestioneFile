07d4ea6824766ea54363fd284361f394733a090e
Marche05 <152077304+Marche05@users.noreply.github.com>
2024-01-24 18:29:14 +0100

    Add files via upload
    ISSUE 1-2

---
+++ src/gestionefile/GestioneFile.java
@@ -0,0 +1,52 @@
+package gestionefile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.Scanner;
+
+/**
+ *
+ * @author MC
+ * @version 12/01/23
+ */
+public class GestioneFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //1)LETTURA
+        Lettore lettore = new Lettore("user.json");
+        lettore.start();
+        //2)ELABORAZIONE
+        
+        Scanner s = new Scanner(System.in);
+	
+        System.out.print("Inserisci username: ");
+        String utente = s.nextLine();
+      	
+        System.out.print("Inserisci password: ");
+        String password = s.nextLine();
+        
+        //3) SCRITTURA
+        Scrittore scrittore = new Scrittore("output.csv", utente, password);
+        Thread threadScrittore = new Thread(scrittore);
+        threadScrittore.start();
+                
+        // Utilizzo oggetto Path per indicare il persorso dei file necessario per la copia
+        Path copia = Paths.get("output.csv");
+        Path incolla = Paths.get("copia.csv");
+        
+        try {
+            // Copio file come indicato in https://docs.oracle.com/javase/tutorial/essential/io/copy.html
+            Files.copy(copia, incolla, REPLACE_EXISTING);
+        } catch (IOException ex) {
+            System.err.println("Errore in copia!");
+        }
+    }
+    
+}
---
+++ src/gestionefile/Lettore.java
@@ -0,0 +1,46 @@
+package gestionefile;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author MC
+ * @ 12/01/23
+ */
+
+public class Lettore extends Thread{
+    String nomeFile;
+    
+    public Lettore(String nomeFile){
+        this.nomeFile = nomeFile;
+    }
+    
+    /**
+     * Legge il file senza tener conto del tipo di file
+     * e lo mostra in output
+     */
+    public void leggi(){
+        int i; 
+        
+        // try with resources 
+        // dichiaro risorse dentro le parentesi tonde del try così si chiudono in automatico dopo l'esecuzione
+        // non scrivo fr.close()
+        try (FileReader fr = new FileReader(nomeFile)) { 
+            
+            //2) leggo carattere per carattere e lo stampo 
+            while ((i=fr.read()) != -1)
+                System.out.print((char) i);
+            
+            System.out.print("\n\r");
+
+        } catch (IOException ex) {
+            System.err.println("Errore in lettura!");
+        }
+    }
+    
+
+    public void run(){
+        leggi();
+    }
+}
